11eda03681ad21cb63997a5d370adc5117ddc7f4
maliubiao <maliubiao@gmail.com>
2014-10-05 18:58:14 +0800

    use O(1) hashmap instead of O(n) list

--- hotkey.py
+++ hotkey.py
@@ -4,8 +4,9 @@ import select
 import os
 import pwd
 import fcntl
+import signal
 import os.path
-
+import hashlib
 
 def run_as_user(user):
     try:
@@ -51,8 +52,59 @@ def find_kbds():
     return ["/dev/input/"+x for x in r] 
 
 
+def keys_hash(keys):
+    return hashlib.md5("".join([chr(x).zfill(3) for x in sorted(keys)])).digest()
+
+
+def keys_table(CONFIG):
+    ret = {}
+    for k,v in CONFIG:
+        ret[keys_hash(k)] = v
+    return ret
+
+
+def poll(pollobj, fobj, pressed_now, nsec): 
+    for fd, event in pollobj.poll(nsec): 
+        #make sure no zombies
+        try:
+            os.wait3(os.WNOHANG)
+        except OSError:
+            pass
+        if event & select.EPOLLIN:
+            n = keyboard.parse_event(fobj)
+            if DUMP:  
+                d = n.copy()
+                d["code"] = keyboard.key_table[n["code"]]
+                d["type"] = keyboard.ev_table[n["type"]] 
+                print d 
+            if n["type"] == keyboard.EV_KEY: 
+                if n["value"] >= keyboard.PRESSED:
+                    pressed_now[n["code"]] = n["tv_sec"]
+                elif n["value"] == keyboard.RELEASED:
+                    if n["code"] in pressed_now:
+                        del pressed_now[n["code"]] 
+            if not pressed_now:
+                continue
+            keys = sorted(pressed_now.keys())
+            h = keys_hash(keys)
+            if h in KEYS_TABLE:
+                action = KEYS_TABLE[h]
+                #run as daemon
+                if not os.fork(): 
+                    os.setsid()
+                    if not os.fork():
+                        print "run:", action
+                        os.execvp("/bin/sh", ["sh", "-c", action]) 
+                    os.wait()
+                    exit(0)
+                #trigger once
+                for k in keys:
+                    del pressed_now[k] 
+        if event & select.EPOLLERR: 
+            exit(1) 
+
 def run_listener(device): 
-    pressed_now = {} 
+    pressed_now = {}
     f = open(device, "r") 
     x = select.epoll() 
     x.register(f.fileno(), select.EPOLLIN|select.EPOLLERR) 
@@ -65,47 +117,16 @@ def run_listener(device):
     os.environ["HOME"] = home
     os.environ["XAUTHORITY"] = home + "/.Xauthority" 
     while True:
-        for fd, event in x.poll(1): 
-            #make sure no zombies
-            try:
-                os.wait3(os.WNOHANG)
-            except OSError:
-                pass
-            if event & select.EPOLLIN:
-                n = keyboard.parse_event(f)
-                if DUMP:  
-                    d = n.copy()
-                    d["code"] = keyboard.key_table[n["code"]]
-                    d["type"] = keyboard.ev_table[n["type"]] 
-                    print d 
-                if n["type"] == keyboard.EV_KEY: 
-                    if n["value"] >= keyboard.PRESSED:
-                        pressed_now[n["code"]] = n["tv_sec"]
-                    elif n["value"] == keyboard.RELEASED:
-                        if n["code"] in pressed_now:
-                            del pressed_now[n["code"]] 
-                for key in CONFIG:
-                    ks, action = key
-                    if all([k in pressed_now for k in ks]): 
-                        #run as daemon
-                        if not os.fork(): 
-                            os.setsid()
-                            if not os.fork():
-                                print "run:", action
-                                os.execvp("/bin/sh", ["sh", "-c", action]) 
-                            os.wait()
-                            exit(0)
-                        #trigger once
-                        for k in ks:
-                            del pressed_now[k]
-                        #pressed_now = {}
-            if event & select.EPOLLERR: 
-                exit(1) 
+        try:
+            poll(x, f, pressed_now, 1)
+        except IOError as e:
+            pass
 
 def hotkey(kbds): 
     #open keyboard 
     table = {}
     for i in kbds:
+        print "kbd: ", i
         pid = os.fork()
         if not pid: 
             run_listener(i)
@@ -120,9 +141,11 @@ def hotkey(kbds):
             exit(0) 
         except KeyboardInterrupt:
             print "die" 
-            exit(0)
+            exit(0) 
 
 
 if __name__ == "__main__":
+    import pdb
     from hotkey_config import CONFIG, DUMP, USER 
+    KEYS_TABLE = keys_table(CONFIG) 
     hotkey(find_kbds()) 
